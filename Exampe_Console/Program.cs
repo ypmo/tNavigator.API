73c670d9c4bb2caa8cbb5131772776029c8cbabe
Sergey Ilin <silyinnur@gmail.com>
2025-03-30 08:19:08 +0500

    EXAMPLE

---
+++ Exampe_Console/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string tNpath = "~/tNavigator/tNavigator-con";
+
+if (!Path.Exists(tNpath))
+{
+    Console.WriteLine($"Console tNavigator not found at {Path.GetFullPath(tNpath)}!");
+    Environment.Exit(1);
+}
+
+if (!Path.Exists("Init_Data"))
+{
+    Console.WriteLine($"Init_Data folder not found!");
+    Environment.Exit(1);
+}
