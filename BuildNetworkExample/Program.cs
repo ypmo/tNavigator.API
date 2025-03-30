02290e70ef01ba867add0a48a0c9dd0bef46ee2e
Sergey Ilin <silyinnur@gmail.com>
2025-03-30 09:54:43 +0500

    example project

--- BuildNetworkExample/Program.cs
+++ BuildNetworkExample/Program.cs
@@ -12,3 +12,5 @@ if (!Path.Exists("Init_Data"))
     Console.WriteLine($"Init_Data folder not found!");
     Environment.Exit(1);
 }
+
+List<string> xls_list = ["Init_Data/WD_data.xlsx", "Init_Data/ND_data.xlsx"];
