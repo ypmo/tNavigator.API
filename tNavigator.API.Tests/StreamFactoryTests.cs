8345cb1d4c42f21143e26a0377436c061f10ec89
Sergey Ilyin <silyinnur@gmail.com>
2025-04-07 23:03:00 +0500

    streamFactory test

--- tNavigator.API.Tests/StreamFactoryTests.cs
+++ tNavigator.API.Tests/StreamFactoryTests.cs
@@ -7,10 +7,10 @@ public class StreamFactoryTests
     [Fact]
     public void StreamFactoryCanCreateStream()
     {
-        var stream = StreamFactory.ASCII2ByteStream("01");
-        var buffer = new byte[1];
+        var stream = StreamFactory.ASCII2ByteStream("e4");
+        var buffer = new byte[2];
         var data = stream.Read(buffer);
         Assert.Equal(1, data);
-        Assert.Equal(1, buffer[0]);
+        Assert.Equal(228, buffer[0]);
     }
 }
--- tNavigator.API.Tests/StreamReaderTests.cs
+++ tNavigator.API.Tests/StreamReaderTests.cs
@@ -6,7 +6,5 @@ public class StreamReaderTests
     public void Test1()
     {
 
-    }
-
-    
+    }    
 }
