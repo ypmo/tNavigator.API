8e990697dc7b1498b7d5a262bff1733b4288fe6f
Sergey Ilyin <silyinnur@gmail.com>
2025-04-09 19:49:35 +0500

    bad

--- tNav.Common.Tests/StreamParserTests.cs
+++ tNav.Common.Tests/StreamParserTests.cs
@@ -8,9 +8,21 @@ public class StreamParserTests
     [InlineData("04000000000000004e6f6e65", null)]
     public void ParseString(string indata, string? outdata)
     {
-        var stream = StreamFactory.ASCII2ByteStream(indata);
-        var parsed = StreamParser.unpack_data(stream);
+
+        using var stream = GenerateStreamFromString(indata);
+        using var reader = new StreamReader(stream);
+        var parsed = StreamParser.unpack_data(reader);
         // Assert.Equal(parsed, outdata);
         // Assert.Equal(parsed, outdata);
-    }   
+    }
+
+    static Stream GenerateStreamFromString(string s)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(s);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
 }
