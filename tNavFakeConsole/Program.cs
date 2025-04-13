78662557f489d650a2dc7e9c63fe946bf4d85a51
Sergey Ilin <silyinnur@gmail.com>
2025-04-14 01:08:13 +0500

    has multy lines problem

--- tNavFakeConsole/Program.cs
+++ tNavFakeConsole/Program.cs
@@ -12,11 +12,30 @@ var queries = LogParser.Parse(logText);
 
 foreach (var query in queries)
 {
-    //using var sr = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding);
-    //using var sw = new StreamWriter(Console.OpenStandardOutput(), Console.OutputEncoding);
-    var input = Console.ReadLine();
+    List<string> lines = [];
+
+    log.Info("начинаем чтение строки");
+    bool firstLine = true;
+    bool endFinded = true;
+    while (firstLine || !endFinded)
+    {
+        var line = Console.ReadLine();
+        if (firstLine && line.EndsWith("= \""))
+        {
+            endFinded = false;
+        }
+        else if (!firstLine && line.EndsWith("\")"))
+        {
+            endFinded = true;
+        }
+        firstLine = false;
+        log.Info($"Считали {line}");
+        lines.Add(line);
+    }
+    log.Info("Закончили чтение");
+    var input = string.Join('\n', lines);
     log.Info("***Получили***", input);
-    if (!string.Equals(query.Query.Trim('\n'), input))
+    if (!string.Equals(query.Query.Trim('\n'), input.Trim('\n')))
     {
         List<string> content = [];
         content.Add("***ожидалось***");
@@ -24,13 +43,12 @@ foreach (var query in queries)
         content.Add("***получили***");
         content.Add(input);
         log.Error(content.ToArray());
-        throw new Exception("Not equals");
-
+        //throw new Exception("Not equals");
     }
     foreach (var responce in query.Responses)
     {
         ///Console.OpenStandardOutput().Write(responce.Data)
-         log.Info("***ОТВЕТ***", responce.Data);
+        log.Info("***ОТВЕТ***", responce.Data);
         Console.Write(responce.Data);
 
         //Console.WriteLine(responce);
