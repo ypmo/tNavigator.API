b9e5c8ad05d38a14b7462652059f5650eb9dde9b
Sergey Ilin <silyinnur@gmail.com>
2025-04-13 12:02:56 +0500

    Read Time

--- BuildNetworkExample/Program.cs
+++ BuildNetworkExample/Program.cs
@@ -1,16 +1,12 @@
-﻿#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
-using Microsoft.Data.Analysis;
-using Microsoft.VisualBasic;
+﻿using Microsoft.Data.Analysis;
 using System.Data;
 using System.Linq;
-using System.Runtime.Intrinsics.X86;
 using System.Xml.Linq;
 using tNav.API;
 using tNav.Common;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 using tnav = tNav.API;
-#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
 
+System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
 Console.Write("Путь к консоли tNavigator (tNavigator-con)");
 string tNpath = Console.ReadLine() ?? "";
 if (string.IsNullOrEmpty(tNpath))
@@ -36,7 +32,7 @@ foreach (var xls in xls_list)
     var cvses = ExcelHelper.ExcelToCSV(xls, [], 1);
     foreach (var (name, content) in cvses)
     {
-        var frame = DataFrame.LoadCsvFromString(content, separator: ',', header: true, cultureInfo: System.Globalization.CultureInfo.InvariantCulture);
+        var frame = DataFrame.LoadCsvFromString(content, separator: ',', header: true);
         df_data.Add(name, frame);
     }
     //df_data.update(pd.read_excel(xls, engine: "openpyxl", sheet_name: null, skiprows: 1, keep_default_na: false));
@@ -51,13 +47,14 @@ List<string> quoted_names = ["name", "perforation_status", "poro_system", "statu
 List<string> datetime_names = ["time_step", "event_date"];
 
 List<DateTime> timestamps = [];
-foreach (var df in df_data.Values)
+foreach (var df_par in df_data)
 {
+    var df = df_par.Value;
     foreach (var colName in datetime_names)
     {
         if (df.Columns.Any(t => t.Name == colName))
         {
-            var values = df[colName].Cast<DateTime>().Distinct();
+            var values = df.Columns[colName].Cast<DateTime>().Distinct().ToArray();
             timestamps.AddRange(values);
         }
         timestamps = timestamps.OrderBy(t => t).ToList();
--- tNav.Common.Tests/ExcelHelperTests.cs
+++ tNav.Common.Tests/ExcelHelperTests.cs
@@ -28,4 +28,24 @@ public class ExcelHelperTests
         Assert.NotNull(content);
         DataFrame df = DataFrame.LoadCsvFromString(content);
     }
+
+
+    [Fact]
+    public void ReadDateTimeDataFrame()
+    {
+        System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+        List<(string name, string content)> csvs = ExcelHelper.ExcelToCSV("ExcelData.xlsx", ["Chokes"], 1);
+        var name = csvs[0].name;
+        var content = csvs[0].content;
+        Assert.NotNull(content);
+        DataFrame df = DataFrame.LoadCsvFromString(content);
+        foreach(var row in df.Rows)
+        {
+            var dt=row["time_step"];
+            Assert.IsType<DateTime>(dt);
+        }
+        var value=df.Rows[0]["time_step"];
+        Assert.True(value is DateTime);
+        Assert.Equal(value, new DateTime(2024,03,14));
+    }
 }
