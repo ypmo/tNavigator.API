384fb3c9d696b35b1949852e9038f2fc3f71ee4a
Sergey Ilin <silyinnur@gmail.com>
2025-11-05 13:55:17 +0500

    start options

--- BuildNetworkExample/Program.cs
+++ BuildNetworkExample/Program.cs
@@ -1,13 +1,44 @@
 ﻿using BuildNetworkExample;
 using Microsoft.Data.Analysis;
+using System.CommandLine;
+using System.CommandLine.Parsing;
 using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Xml.Linq;
 
-System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
 
-tNavSettings settings = new tNavSettings();
+System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+tNavSettings settings = new();
+string? variant = null;
+
+Option<FileInfo> tNavPathOption = new("--tNavPath")
+{
+    Description = "Путь к приложению 'консоль тНавигатор'"
+};
+
+Option<string> actionOption = new("--action")
+{
+    Description = "автоматическое выполнение пункта меню"
+};
+
+var rootCommand = new RootCommand("Example tNavigator Console tool");
+rootCommand.Options.Add(tNavPathOption);
+rootCommand.Options.Add(actionOption);
+ParseResult parseResult = rootCommand.Parse(args);
+
+if (parseResult.Errors.Count != 0)
+{
+    foreach (ParseError parseError in parseResult.Errors)
+        Console.Error.WriteLine(parseError.Message);
+    Environment.Exit(1);
+}
+
+if (parseResult.GetValue(tNavPathOption) is FileInfo fileInfo)
+    settings.tNavPath = fileInfo.FullName;
+
+if (parseResult.GetValue<string>(actionOption) != null)
+    variant = new FileInfo(settings.tNavPath).FullName;
 
 
 Console.WriteLine($"Текщая директория: {settings.HomePath}");
@@ -27,7 +58,7 @@ Console.WriteLine("4 Пример API1.3_HowToBuildNetworkViaAPIServer new API")
 Console.WriteLine("5 Тест на чтение DataFrame");
 
 
-var variant = Console.ReadLine();
+variant ??= Console.ReadLine();
 while (variant != "0")
 {
     switch (variant)
@@ -40,6 +71,7 @@ while (variant != "0")
             else
                 settings.HomePath = "";
             Console.WriteLine($"Принято:{settings.HomePath}");
+            variant = Console.ReadLine();
             break;
         case "2":
             Console.WriteLine("Путь к tNavigator-con:");
@@ -49,18 +81,22 @@ while (variant != "0")
             else
                 settings.tNavPath = "";
             Console.WriteLine($"Принято:{settings.tNavPath}");
+            variant = Console.ReadLine();
             break;
         case "3":
             new LikePython1_3().Run(settings);
+            variant = "0";
             break;
         case "4":
             new LikePython1_3_NewAPI().Run(settings);
+            variant = "0";
             break;
         case "5":
             new TestRead().ReadFrame(settings);
+            variant = "0";
             break;
     }
-    variant = Console.ReadLine();
+
 }
 Console.WriteLine("Нажмите любую клавишу для выхода");
 Console.ReadKey();
